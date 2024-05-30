5e79523ace303f6a0dfe5dfff1d2392282541fb8
Kadir Kobak <kobakkadir@gmail.com>
2024-05-30 13:02:17 +0300

    Doctor detail panel has been updated.

--- HospitalManagementAutomation/FormDoktorDetay.Designer.cs
+++ HospitalManagementAutomation/FormDoktorDetay.Designer.cs
@@ -32,18 +32,29 @@ namespace HospitalManagementAutomation
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.lblTC = new System.Windows.Forms.Label();
+            this.lblAdSoyad = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.rchSikayet = new System.Windows.Forms.RichTextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.btnBilgiDuzenle = new System.Windows.Forms.Button();
+            this.btnDuyurular = new System.Windows.Forms.Button();
+            this.btnNet = new System.Windows.Forms.Button();
+            this.btnCikis = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.lblAdSoyad);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.lblTC);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
@@ -70,41 +81,111 @@ namespace HospitalManagementAutomation
             this.label2.TabIndex = 0;
             this.label2.Text = "Ad Soyad:";
             // 
-            // label3
+            // lblTC
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(127, 50);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(120, 24);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "00000000000";
+            this.lblTC.AutoSize = true;
+            this.lblTC.Location = new System.Drawing.Point(127, 50);
+            this.lblTC.Name = "lblTC";
+            this.lblTC.Size = new System.Drawing.Size(120, 24);
+            this.lblTC.TabIndex = 0;
+            this.lblTC.Text = "00000000000";
             // 
-            // label4
+            // lblAdSoyad
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(127, 83);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(82, 24);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "Null Null";
+            this.lblAdSoyad.AutoSize = true;
+            this.lblAdSoyad.Location = new System.Drawing.Point(127, 83);
+            this.lblAdSoyad.Name = "lblAdSoyad";
+            this.lblAdSoyad.Size = new System.Drawing.Size(82, 24);
+            this.lblAdSoyad.TabIndex = 0;
+            this.lblAdSoyad.Text = "Null Null";
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.rchSikayet);
             this.groupBox2.Location = new System.Drawing.Point(12, 201);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(312, 466);
+            this.groupBox2.Size = new System.Drawing.Size(312, 320);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Randevu Detay";
             // 
             // groupBox3
             // 
+            this.groupBox3.Controls.Add(this.dataGridView1);
             this.groupBox3.Location = new System.Drawing.Point(343, 12);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(823, 655);
+            this.groupBox3.Size = new System.Drawing.Size(823, 706);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Randevu Listesi";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(817, 675);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // rchSikayet
+            // 
+            this.rchSikayet.Location = new System.Drawing.Point(6, 31);
+            this.rchSikayet.Name = "rchSikayet";
+            this.rchSikayet.Size = new System.Drawing.Size(300, 315);
+            this.rchSikayet.TabIndex = 0;
+            this.rchSikayet.Text = "";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.btnCikis);
+            this.groupBox4.Controls.Add(this.btnNet);
+            this.groupBox4.Controls.Add(this.btnDuyurular);
+            this.groupBox4.Controls.Add(this.btnBilgiDuzenle);
+            this.groupBox4.Location = new System.Drawing.Point(18, 553);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(300, 165);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Hızlı Erişim";
+            // 
+            // btnBilgiDuzenle
+            // 
+            this.btnBilgiDuzenle.Location = new System.Drawing.Point(6, 40);
+            this.btnBilgiDuzenle.Name = "btnBilgiDuzenle";
+            this.btnBilgiDuzenle.Size = new System.Drawing.Size(129, 59);
+            this.btnBilgiDuzenle.TabIndex = 0;
+            this.btnBilgiDuzenle.Text = "Bilgileri Düzenle";
+            this.btnBilgiDuzenle.UseVisualStyleBackColor = true;
+            // 
+            // btnDuyurular
+            // 
+            this.btnDuyurular.Location = new System.Drawing.Point(151, 40);
+            this.btnDuyurular.Name = "btnDuyurular";
+            this.btnDuyurular.Size = new System.Drawing.Size(129, 59);
+            this.btnDuyurular.TabIndex = 0;
+            this.btnDuyurular.Text = "Duyurular";
+            this.btnDuyurular.UseVisualStyleBackColor = true;
+            // 
+            // btnNet
+            // 
+            this.btnNet.Location = new System.Drawing.Point(6, 100);
+            this.btnNet.Name = "btnNet";
+            this.btnNet.Size = new System.Drawing.Size(129, 59);
+            this.btnNet.TabIndex = 0;
+            this.btnNet.Text = "İnternet";
+            this.btnNet.UseVisualStyleBackColor = true;
+            // 
+            // btnCikis
+            // 
+            this.btnCikis.Location = new System.Drawing.Point(151, 100);
+            this.btnCikis.Name = "btnCikis";
+            this.btnCikis.Size = new System.Drawing.Size(129, 59);
+            this.btnCikis.TabIndex = 0;
+            this.btnCikis.Text = "Çıkış";
+            this.btnCikis.UseVisualStyleBackColor = true;
             // 
             // FormDoktorDetay
             // 
@@ -112,6 +193,7 @@ namespace HospitalManagementAutomation
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SeaGreen;
             this.ClientSize = new System.Drawing.Size(1178, 730);
+            this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -121,6 +203,10 @@ namespace HospitalManagementAutomation
             this.Text = "FormDoktorDetay";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -128,11 +214,18 @@ namespace HospitalManagementAutomation
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblAdSoyad;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTC;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox rchSikayet;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button btnCikis;
+        private System.Windows.Forms.Button btnNet;
+        private System.Windows.Forms.Button btnDuyurular;
+        private System.Windows.Forms.Button btnBilgiDuzenle;
     }
 }
