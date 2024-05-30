061f422d0ad8099de71370d77e4c801606f7ca7a
Kadir Kobak <kobakkadir@gmail.com>
2024-05-30 10:26:36 +0300

    patient registration panel updated.

--- HospitalManagementAutomation/FormHastaKayit.Designer.cs
+++ HospitalManagementAutomation/FormHastaKayit.Designer.cs
@@ -35,20 +35,20 @@ namespace HospitalManagementAutomation
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
-            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.mskTCKimlikNo = new System.Windows.Forms.MaskedTextBox();
+            this.mskNumber = new System.Windows.Forms.MaskedTextBox();
+            this.cmbCinsiyet = new System.Windows.Forms.ComboBox();
+            this.btnKayitYap = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(102, 54);
+            this.label1.Location = new System.Drawing.Point(102, 48);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(39, 24);
             this.label1.TabIndex = 0;
@@ -58,7 +58,7 @@ namespace HospitalManagementAutomation
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(73, 81);
+            this.label2.Location = new System.Drawing.Point(73, 75);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(68, 24);
             this.label2.TabIndex = 0;
@@ -68,7 +68,7 @@ namespace HospitalManagementAutomation
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label3.Location = new System.Drawing.Point(16, 114);
+            this.label3.Location = new System.Drawing.Point(16, 108);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(125, 24);
             this.label3.TabIndex = 0;
@@ -78,7 +78,7 @@ namespace HospitalManagementAutomation
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label4.Location = new System.Drawing.Point(63, 145);
+            this.label4.Location = new System.Drawing.Point(63, 139);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(78, 24);
             this.label4.TabIndex = 0;
@@ -88,7 +88,7 @@ namespace HospitalManagementAutomation
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label5.Location = new System.Drawing.Point(87, 176);
+            this.label5.Location = new System.Drawing.Point(87, 170);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(54, 24);
             this.label5.TabIndex = 0;
@@ -98,82 +98,85 @@ namespace HospitalManagementAutomation
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label6.Location = new System.Drawing.Point(58, 211);
+            this.label6.Location = new System.Drawing.Point(58, 205);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(83, 24);
             this.label6.TabIndex = 0;
             this.label6.Text = "Cinsiyet:";
             // 
-            // textBox1
+            // txtAd
             // 
-            this.textBox1.Location = new System.Drawing.Point(146, 49);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(164, 29);
-            this.textBox1.TabIndex = 1;
+            this.txtAd.Location = new System.Drawing.Point(146, 43);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(164, 29);
+            this.txtAd.TabIndex = 1;
             // 
-            // textBox2
+            // txtSoyad
             // 
-            this.textBox2.Location = new System.Drawing.Point(146, 80);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(164, 29);
-            this.textBox2.TabIndex = 1;
+            this.txtSoyad.Location = new System.Drawing.Point(146, 74);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(164, 29);
+            this.txtSoyad.TabIndex = 1;
             // 
-            // textBox3
+            // txtPassword
             // 
-            this.textBox3.Location = new System.Drawing.Point(146, 176);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(164, 29);
-            this.textBox3.TabIndex = 1;
+            this.txtPassword.Location = new System.Drawing.Point(146, 170);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(164, 29);
+            this.txtPassword.TabIndex = 1;
             // 
-            // maskedTextBox1
+            // mskTCKimlikNo
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(146, 112);
-            this.maskedTextBox1.Mask = "00000000000";
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(164, 29);
-            this.maskedTextBox1.TabIndex = 2;
-            this.maskedTextBox1.ValidatingType = typeof(int);
+            this.mskTCKimlikNo.Location = new System.Drawing.Point(146, 106);
+            this.mskTCKimlikNo.Mask = "00000000000";
+            this.mskTCKimlikNo.Name = "mskTCKimlikNo";
+            this.mskTCKimlikNo.Size = new System.Drawing.Size(164, 29);
+            this.mskTCKimlikNo.TabIndex = 2;
+            this.mskTCKimlikNo.ValidatingType = typeof(int);
             // 
-            // maskedTextBox2
+            // mskNumber
             // 
-            this.maskedTextBox2.Location = new System.Drawing.Point(146, 145);
-            this.maskedTextBox2.Mask = "(999) 000-0000";
-            this.maskedTextBox2.Name = "maskedTextBox2";
-            this.maskedTextBox2.Size = new System.Drawing.Size(164, 29);
-            this.maskedTextBox2.TabIndex = 2;
+            this.mskNumber.Location = new System.Drawing.Point(146, 139);
+            this.mskNumber.Mask = "(999) 000-0000";
+            this.mskNumber.Name = "mskNumber";
+            this.mskNumber.Size = new System.Drawing.Size(164, 29);
+            this.mskNumber.TabIndex = 2;
             // 
-            // comboBox1
+            // cmbCinsiyet
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.cmbCinsiyet.FormattingEnabled = true;
+            this.cmbCinsiyet.Items.AddRange(new object[] {
             "Erkek",
             "Kadın"});
-            this.comboBox1.Location = new System.Drawing.Point(146, 209);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(164, 30);
-            this.comboBox1.TabIndex = 3;
+            this.cmbCinsiyet.Location = new System.Drawing.Point(146, 203);
+            this.cmbCinsiyet.Name = "cmbCinsiyet";
+            this.cmbCinsiyet.Size = new System.Drawing.Size(164, 30);
+            this.cmbCinsiyet.TabIndex = 3;
             // 
-            // button1
+            // btnKayitYap
             // 
-            this.button1.Location = new System.Drawing.Point(146, 259);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(164, 43);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnKayitYap.BackColor = System.Drawing.Color.Yellow;
+            this.btnKayitYap.ForeColor = System.Drawing.Color.Black;
+            this.btnKayitYap.Location = new System.Drawing.Point(146, 253);
+            this.btnKayitYap.Name = "btnKayitYap";
+            this.btnKayitYap.Size = new System.Drawing.Size(164, 43);
+            this.btnKayitYap.TabIndex = 4;
+            this.btnKayitYap.Text = "Kayıt Yap";
+            this.btnKayitYap.UseVisualStyleBackColor = false;
             // 
             // FormHastaKayit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(378, 324);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.maskedTextBox2);
-            this.Controls.Add(this.maskedTextBox1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(397, 331);
+            this.Controls.Add(this.btnKayitYap);
+            this.Controls.Add(this.cmbCinsiyet);
+            this.Controls.Add(this.mskNumber);
+            this.Controls.Add(this.mskTCKimlikNo);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -181,7 +184,7 @@ namespace HospitalManagementAutomation
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Corbel", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormHastaKayit";
             this.Text = "FormHastaKayit";
             this.ResumeLayout(false);
@@ -197,12 +200,12 @@ namespace HospitalManagementAutomation
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.MaskedTextBox mskTCKimlikNo;
+        private System.Windows.Forms.MaskedTextBox mskNumber;
+        private System.Windows.Forms.ComboBox cmbCinsiyet;
+        private System.Windows.Forms.Button btnKayitYap;
     }
 }
