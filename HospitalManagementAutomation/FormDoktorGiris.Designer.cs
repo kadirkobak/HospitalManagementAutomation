b7b869f75ce669a4d96e11297d4e3841d46d96df
Kadir Kobak <kobakkadir@gmail.com>
2024-05-30 12:33:19 +0300

    Doctor login panel added.

---
+++ HospitalManagementAutomation/FormDoktorGiris.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace HospitalManagementAutomation
+{
+    partial class FormDoktorGiris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnGirisYap = new System.Windows.Forms.Button();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.mskTCKimlikNo = new System.Windows.Forms.MaskedTextBox();
+            this.LnkUyeOl = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnGirisYap
+            // 
+            this.BtnGirisYap.Location = new System.Drawing.Point(243, 206);
+            this.BtnGirisYap.Name = "BtnGirisYap";
+            this.BtnGirisYap.Size = new System.Drawing.Size(92, 33);
+            this.BtnGirisYap.TabIndex = 13;
+            this.BtnGirisYap.Text = "Giriş Yap";
+            this.BtnGirisYap.UseVisualStyleBackColor = true;
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(243, 153);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(92, 32);
+            this.txtSifre.TabIndex = 12;
+            // 
+            // mskTCKimlikNo
+            // 
+            this.mskTCKimlikNo.Location = new System.Drawing.Point(243, 116);
+            this.mskTCKimlikNo.Mask = "00000000000";
+            this.mskTCKimlikNo.Name = "mskTCKimlikNo";
+            this.mskTCKimlikNo.Size = new System.Drawing.Size(92, 32);
+            this.mskTCKimlikNo.TabIndex = 11;
+            this.mskTCKimlikNo.ValidatingType = typeof(int);
+            // 
+            // LnkUyeOl
+            // 
+            this.LnkUyeOl.AutoSize = true;
+            this.LnkUyeOl.Location = new System.Drawing.Point(356, 161);
+            this.LnkUyeOl.Name = "LnkUyeOl";
+            this.LnkUyeOl.Size = new System.Drawing.Size(66, 24);
+            this.LnkUyeOl.TabIndex = 10;
+            this.LnkUyeOl.TabStop = true;
+            this.LnkUyeOl.Text = "Üye Ol";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(191, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 24);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Şifre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(121, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "TC Kimlik No:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(116, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 57);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Doktor Giriş Paneli";
+            // 
+            // FormDoktorGiris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(554, 264);
+            this.Controls.Add(this.BtnGirisYap);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.mskTCKimlikNo);
+            this.Controls.Add(this.LnkUyeOl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FormDoktorGiris";
+            this.Text = "FormDoktorGiris";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnGirisYap;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.MaskedTextBox mskTCKimlikNo;
+        private System.Windows.Forms.LinkLabel LnkUyeOl;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
