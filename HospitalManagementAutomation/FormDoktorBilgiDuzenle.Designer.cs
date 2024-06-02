5e2c929296aab6d8547eebcddf89e06647edb7a6
Kadir Kobak <kobakkadir@gmail.com>
2024-06-02 16:15:17 +0300

    Updated information editing panel for doctor.

--- HospitalManagementAutomation/FormDoktorBilgiDuzenle.Designer.cs
+++ HospitalManagementAutomation/FormDoktorBilgiDuzenle.Designer.cs
@@ -29,21 +29,156 @@ namespace HospitalManagementAutomation
         /// </summary>
         private void InitializeComponent()
         {
+            this.mskTCKimlikNo = new System.Windows.Forms.MaskedTextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbBrans = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnBilgiGuncelle = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // mskTCKimlikNo
+            // 
+            this.mskTCKimlikNo.Location = new System.Drawing.Point(149, 173);
+            this.mskTCKimlikNo.Mask = "00000000000";
+            this.mskTCKimlikNo.Name = "mskTCKimlikNo";
+            this.mskTCKimlikNo.Size = new System.Drawing.Size(164, 32);
+            this.mskTCKimlikNo.TabIndex = 23;
+            this.mskTCKimlikNo.ValidatingType = typeof(int);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(149, 252);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(164, 32);
+            this.txtPassword.TabIndex = 20;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(149, 136);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(164, 32);
+            this.txtSoyad.TabIndex = 21;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(149, 99);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(164, 32);
+            this.txtAd.TabIndex = 22;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(90, 252);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 24);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Şifre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(19, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 24);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "TC Kimlik No:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(76, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 24);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Soyad:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(105, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 24);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Ad:";
+            // 
+            // cmbBrans
+            // 
+            this.cmbBrans.FormattingEnabled = true;
+            this.cmbBrans.Location = new System.Drawing.Point(149, 212);
+            this.cmbBrans.Name = "cmbBrans";
+            this.cmbBrans.Size = new System.Drawing.Size(164, 32);
+            this.cmbBrans.TabIndex = 24;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(18, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 24);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Doktor Branş:";
+            // 
+            // btnBilgiGuncelle
+            // 
+            this.btnBilgiGuncelle.BackColor = System.Drawing.Color.Yellow;
+            this.btnBilgiGuncelle.ForeColor = System.Drawing.Color.Black;
+            this.btnBilgiGuncelle.Location = new System.Drawing.Point(149, 301);
+            this.btnBilgiGuncelle.Name = "btnBilgiGuncelle";
+            this.btnBilgiGuncelle.Size = new System.Drawing.Size(164, 43);
+            this.btnBilgiGuncelle.TabIndex = 25;
+            this.btnBilgiGuncelle.Text = "Güncelle";
+            this.btnBilgiGuncelle.UseVisualStyleBackColor = false;
             // 
             // FormDoktorBilgiDuzenle
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1100, 675);
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(365, 408);
+            this.Controls.Add(this.btnBilgiGuncelle);
+            this.Controls.Add(this.cmbBrans);
+            this.Controls.Add(this.mskTCKimlikNo);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormDoktorBilgiDuzenle";
             this.Text = "FormDoktorBilgiDuzenle";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MaskedTextBox mskTCKimlikNo;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbBrans;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnBilgiGuncelle;
     }
 }
