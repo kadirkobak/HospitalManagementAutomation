3669a7a2a355871b3aba25d56b4909d5ba5dbf18
Kadir Kobak <kobakkadir@gmail.com>
2024-06-02 16:33:56 +0300

    Secretary detail panel added.

---
+++ HospitalManagementAutomation/FormSekreterDetay.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace HospitalManagementAutomation
+{
+    partial class FormSekreterDetay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormSekreterDetay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 675);
+            this.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FormSekreterDetay";
+            this.Text = "FormSekreterDetay";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
