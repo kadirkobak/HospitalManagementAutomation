feec0cf2fb78326bb3e22cab15efe242fe788a5e
Kadir Kobak <kobakkadir@gmail.com>
2024-06-02 18:16:49 +0300

    Secretary detail panel updated.

--- HospitalManagementAutomation/FormSekreterDetay.Designer.cs
+++ HospitalManagementAutomation/FormSekreterDetay.Designer.cs
@@ -35,7 +35,8 @@ namespace HospitalManagementAutomation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1100, 675);
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(1160, 741);
             this.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormSekreterDetay";
